b9a928ff739804d668cb72b8d02adda9fe5fcdf8
amosmurmu <amosmurmu002@gmail.com>
2025-04-19 22:42:53 +0530

    chore: database test connection

---
+++ src/config/db.ts
@@ -0,0 +1,26 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseKey = process.env.SUPABASE_KEY || '';
+
+export const supabase = createClient(
+    supabaseUrl,
+    supabaseKey
+)
+export async function testConnection(){
+    try {
+        const {data, error} = await supabase.from("users")
+        .select('*')
+        .limit(1);
+
+        if(error) throw error;
+        console.log('Supabase connection successful');
+        return { success: true, data}
+    } catch (error) {
+        console.error('Supabase connection failed:', error);
+        return { sucess: false, error};
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 
 import uploadRoutes from "./routes/upload.route";
-
+import {testConnection} from "./config/db";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -13,6 +13,7 @@ app.use(express.json()); // Middleware to parse JSON
 app.use(express.static("public")); // Serve static files
 app.use("/api", uploadRoutes);
 
+
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
   res.status(err.status || 500).json({
@@ -20,6 +21,13 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on  `, { PORT });
-});
+(async ()=>{
+  const result = await testConnection();
+  if(!result.success){
+    console.error("Supabase test failed, Exiting...");
+    process.exit(1);
+  }
+  app.listen(PORT, () => {
+    console.log(`Server running on  `, { PORT });
+  });
+})();
